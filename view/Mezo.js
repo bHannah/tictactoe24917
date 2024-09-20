0db5169cae760e051685c9422ff2bf3617cff619
bHannah <borsikaaa1@gmail.com>
2024-09-20 21:45:38 +0200

    a játéktábla négyzetei kattinthatók, és visszaadják a jó indexet

--- view/Mezo.js
+++ view/Mezo.js
@@ -8,6 +8,8 @@ export default class Mezo{
         this.#index=index
         this.szuloElem = szuloElem;
         this.mezoMegjelenit()
+        this.kattintottElem = $(".kartya:last-child");
+        this.esemenyKezelo()
     }
     //tagfüggvények
     mezoMegjelenit(){
@@ -15,5 +17,13 @@ export default class Mezo{
             <div class="card col-sm kartya" id="box">
             <p class="card-text">${this.#lepes}</p>
             </div>`)
+    }
+
+    esemenyKezelo(){
+        this.kattintottElem.on("click", ()=>{
+            const e = new CustomEvent("kattintas", {detail: this.#index});
+            window.dispatchEvent(e);
+            console.log(e);
+        })
     }
 }
